9ac066a4b6b689856f2d85080c1b8c54dc3d7339
haocheng <15034004459@163.com>
2019-08-27 14:23:59 +0800

    初始化导入

---
+++ src/main/java/com/haocheng/Application.java
@@ -0,0 +1,15 @@
+package com.haocheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: haocheng
+ * @Date: 2019-08-27 14:01
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
