6d25faeb858482cd35d50758a4825ee34e4e6536
haocheng <15034004459@163.com>
2019-10-14 14:47:27 +0800

    perf: 添加PageHelper分页

---
+++ src/main/java/com/haocheng/study/util/paging/PageRequest.java
@@ -0,0 +1,33 @@
+package com.haocheng.study.util.paging;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 分页查询请求封装类
+ *
+ * @Author: haocheng
+ * @Date: 2019-10-10 15:48
+ */
+@Data
+public class PageRequest implements Serializable {
+
+    /**
+     * 当前页码
+     */
+    private int pageNum = 1;
+
+    /**
+     * 每页数量
+     */
+    private int pageSize = 10;
+
+    /**
+     * 排序的字段及顺序
+     */
+    private String orderBy;
+
+
+
+}
---
+++ src/main/java/com/haocheng/study/util/paging/PageResult.java
@@ -0,0 +1,42 @@
+package com.haocheng.study.util.paging;
+
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * 分页查询结果封装类
+ *
+ * @Author: haocheng
+ * @Date: 2019-10-10 15:50
+ */
+@Data
+public class PageResult<T> {
+
+//    当前页码
+//    private int pageNum;
+
+//    每页数量
+//    private int pageSize;
+
+//    页码总数
+//    private int totalPages;
+
+    /**
+     * 记录总数
+     */
+    private long totalSize;
+    /**
+     * 数据集
+     */
+    private List<T> content;
+
+    public PageResult(){
+    }
+
+    public PageResult(Long totalSize, List<T> content) {
+        this.totalSize = totalSize;
+        this.content = content;
+    }
+
+}
